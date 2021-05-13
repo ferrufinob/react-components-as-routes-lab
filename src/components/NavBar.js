b980431addb650705860beb6226acdfd1adad8c4
Brenda Ferrufino <ferrufinob@gmail.com>
2021-05-12 23:02:17 -0400

    commit tree 30ca2a411a0a2c3d908a142dbd46fcafe2ef40a0

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+  const links = {
+    color: "white",
+    background: "grey",
+    textDecoration: "none",
+    width: "100px",
+    padding: "12px",
+  };
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        style={links}
+        activeStyle={{ background: "darkgrey" }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        style={links}
+        activeStyle={{ background: "darkgrey" }}
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        style={links}
+        activeStyle={{ background: "darkgrey" }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        style={links}
+        activeStyle={{ background: "darkgrey" }}
+      >
+        Actors
+      </NavLink>
     </div>
   );
 };
